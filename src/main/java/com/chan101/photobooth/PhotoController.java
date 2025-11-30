105521d994dedaf96b07a4a167c6d9cee439a955
chan101 <chandankumarchan1999@gmail.com>
2025-11-30 23:43:52 +0200

    * Add path variables to all methods. * Add PUT method which uploads files. * Add POST method which creates new directory. * Add DELETE method which deletes files.

--- src/main/java/com/chan101/photobooth/PhotoController.java
+++ src/main/java/com/chan101/photobooth/PhotoController.java
@@ -1,7 +1,11 @@
 package com.chan101.photobooth;
 
+import java.awt.image.ImageProducer;
 import java.io.File;
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -9,13 +13,18 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequestMapping("/photos")
@@ -24,10 +33,11 @@ public class PhotoController {
     @Value("${app.images.path}")
     private String imagesPath;
 
-	@GetMapping
-	public ResponseEntity<?> getPhotos() {
+	@GetMapping("/{*subPath}")
+	public ResponseEntity<?> getPhotos(@PathVariable String subPath) {
+		
 
-	    File directory = new File(imagesPath);
+	    File directory = new File(imagesPath+subPath);
 
 	    // If directory doesn't exist → return 404
 	    if (!directory.exists() || !directory.isDirectory()) {
@@ -46,7 +56,6 @@ public class PhotoController {
 	    for (File file : filesList) {
 	    	JSONObject obj1 = new JSONObject();
 			if(file.isDirectory()) {
-				
 				obj1.put("type", "D");
 				obj1.put("file", file.getPath().replace(imagesPath, "images/"));
 			}
@@ -63,14 +72,54 @@ public class PhotoController {
 	}
 
 
+    @PutMapping(value = "/{*subPath}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadFile(@PathVariable String subPath, @RequestParam("file") MultipartFile file) {
 
-    @PostMapping
-    public String uploadPhoto() {
-        return "Photo uploaded!";
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "File is empty."));
+        }
+
+        try {
+            Path targetPath = Paths.get(imagesPath + subPath, file.getOriginalFilename());
+
+            Files.write(targetPath, file.getBytes());
+
+            return ResponseEntity.ok().body(Map.of("message", "Upload successful"));
+
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body(Map.of("message", e.toString()));
+        }
+    }
+    
+    @PostMapping(value = "/{*subPath}")
+    public ResponseEntity<?> createDirectory(@PathVariable String subPath){
+    	File newDir = new File(imagesPath, subPath);
+    	if(newDir.mkdir()) {
+    		return ResponseEntity.ok().body(Map.of("message","Folder created successfully"));
+    	}
+    	else {
+    		return ResponseEntity.internalServerError().body(Map.of("message","Folder could not be created"));
+    	}
+		
     }
 
-    @DeleteMapping("/{id}")
-    public String deletePhoto(@PathVariable String id) {
-        return "Photo " + id + " deleted!";
+    @DeleteMapping("/{*subPath}")
+    public ResponseEntity<?> deletePhoto(@PathVariable String subPath) {
+    	
+    	
+    	File file = new File(imagesPath+subPath);
+    	
+    	if(!file.exists()) {
+    		return ResponseEntity.badRequest().body(Map.of("message","File does not exist"));
+    	}
+    	
+    	if(file.delete()) {
+    		return ResponseEntity.ok().body(Map.of("message","File "+ subPath +" deleted successfully"));
+    	}
+    	else {
+    		return ResponseEntity.badRequest().body(Map.of("message","File "+ subPath +" could not be deleted successfully"));
+    	}
+    	
     }
 }
